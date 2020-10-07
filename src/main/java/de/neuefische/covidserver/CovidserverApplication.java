9ad675996d57a46f944eeda0ace0921b9c92e576
Rikardo <rikardo@marenzzi.de>
2020-10-07 10:39:48 +0200

    Initial commit

---
+++ src/main/java/de/neuefische/covidserver/CovidserverApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.covidserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CovidserverApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/covidserver/CovidserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.covidserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
